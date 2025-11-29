861c1e41d605f3c755f20e1d26baff9c65698c04
Paricherh Abedzadeh <Parichehrabedzadeh@gmail.com>
2025-11-29 21:47:25 +0330

    bug:fixed

--- my-admin-panel/src/Components/Products/ManagmentProducts/ConfirmDialog.jsx
+++ my-admin-panel/src/Components/Products/ManagmentProducts/ConfirmDialog.jsx
@@ -1,12 +1,11 @@
 import styles from "./ConfirmDialog.module.css";
-import deletes from "../../../assets/icons/Close.png";
 
 function ConfirmDialog({ open,message, onConfirm, onCancel }) {
   if (!open) return null;
   return (
     <div className={styles.backdrop} onClick={onCancel}>
       <div className={styles.dialog} onClick={(e) => e.stopPropagation()}>
-        <img src={deletes} alt="بسته شدن" />
+        <img src="/public/Close.png" alt="بسته شدن" />
         <p>{message}</p>
         <div className={styles.actions}>
           <button className={styles.no} onClick={onConfirm}>حذف</button>
